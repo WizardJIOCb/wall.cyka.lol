7ef9440b3891c38be1ea5cb7e4bbbd9353662c64
Rodion <rodion89@list.ru>
2025-11-07 00:50:13 +0500

    add stuff

---
+++ public/assets/RegisterView-DTZseLnn.js
@@ -0,0 +1,2 @@
+import e from"./LoginView-BBVDUYyp.js";import{f as o,z as r,i as t}from"./vendor-B5QdKGWw.js";import"./index-DBwaF-Ry.js";import"./utils-i9HKr1_Q.js";const _=o({__name:"RegisterView",setup(i){return(n,p)=>(t(),r(e))}});export{_ as default};
+//# sourceMappingURL=RegisterView-DTZseLnn.js.map
